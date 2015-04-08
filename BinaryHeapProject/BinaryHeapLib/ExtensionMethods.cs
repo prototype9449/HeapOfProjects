c2b0800762bbea4d5adbdc414c5ed23dc5daa8d2
prototype9449 <prototype9449@yandex.ru>
2015-04-08 23:17:51 +0300

    Project Binary Heap was added

---
+++ BinaryHeapProject/BinaryHeapLib/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace BinaryHeapLib
+{
+    public static class ExtensionMethods
+    {
+        public static void Swap<T>(this T[] elements, int i, int j)
+        {
+            var temp = elements[i];
+            elements[i] = elements[j];
+            elements[j] = temp;
+        }
+        public static void Swap<T>(this List<T> elements, int i, int j)
+        {
+            var temp = elements[i];
+            elements[i] = elements[j];
+            elements[j] = temp;
+        }
+    }
+}
---
+++ BinaryHeapProject/TestBinaryHeap/TestAdding.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using BinaryHeapLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBinaryHeap
+{
+    [TestClass]
+    public class TestAdding
+    {
+        private int[] GetArray()
+        {
+
+            var array = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            return array;
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var heap = new BinaryHeap<int>(GetArray()).GetSortedArray();
+            var array = GetArray().Reverse().ToArray();
+            for (int i = 0; i < heap.Length; i++)
+            {
+                Assert.AreEqual(heap[i], array[i]);
+            }
+
+        }
+    }
+}
