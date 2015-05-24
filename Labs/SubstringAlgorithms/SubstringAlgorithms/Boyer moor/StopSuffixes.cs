ef7b937490c5b14b990610c6ed8ac2d7911966b8
prototype9449 <prototype9449@yandex.ru>
2015-05-24 13:24:22 +0300

    Переделал курсовую

--- Labs/SubstringAlgorithms/SubstringAlgorithms/Boyer moor/StopSuffixes.cs
+++ Labs/SubstringAlgorithms/SubstringAlgorithms/Boyer moor/StopSuffixes.cs
@@ -21,18 +21,39 @@ namespace SubstringAlgorithms.Boyer_moor
 
         private void BuildStopSuffixes()
         {
-            for (int i = _sample.Length-1; i >= 0 ; i--)
+
+            if (_sample.Length == 1)
+            {
+                _stopSuffixes.Add(_sample, _providerStopSymbols[_sample[_sample.Length - 1]]);
+                return;
+            }
+
+            _stopSuffixes.Add(_sample[_sample.Length-1].ToString(), _providerStopSymbols[_sample[_sample.Length - 1]]);
+
+            for (int i = _sample.Length - 2; i >= 0; i--)
             {
                 var suffix = _sample.Substring(i, _sample.Length - i);
                 var other = _sample.Substring(0, i);
                 var position = other.LastIndexOf(suffix);
                 if (position == -1)
                 {
-                    Add(suffix, _sample.Length);
+                    var subSuffix = suffix;
+                    for (int j = 1; j < suffix.Length && (_sample.IndexOf(subSuffix) != 0 || subSuffix.Length == _sample.Length) && subSuffix.Length != 0; j++)
+                    {
+                        subSuffix = subSuffix.Substring(1, subSuffix.Length - 1);
+                    }
+                    if (subSuffix.Length != 0)
+                    {
+                        Add(suffix, _stopSuffixes[subSuffix]);
+                    }
+                    else
+                    {
+                        Add(suffix, _sample.Length);
+                    }
                 }
                 else
                 {
-                    Add(suffix,_sample.Length-position-suffix.Length);
+                    Add(suffix, _sample.Length - position - suffix.Length);
                 }
             }
         }
@@ -51,35 +72,7 @@ namespace SubstringAlgorithms.Boyer_moor
 
         public int this[string suffix]
         {
-            get
-            {
-                if (_stopSuffixes.ContainsKey(suffix))
-                {
-                    var subSuffix = suffix;
-                    for (int i = 1; i < suffix.Length && (_sample.IndexOf(subSuffix) != 0 || subSuffix.Length == _sample.Length) && subSuffix.Length != 0; i++)
-                    {
-                        subSuffix = subSuffix.Substring(1, subSuffix.Length - 1);
-                    }
-                    //while (_sample.IndexOf(subSuffix) != 0 && subSuffix.Length != 0)
-                    //{
-                    //    subSuffix = subSuffix.Substring(0, subSuffix.Length - 1);
-                    //}
-                    if (subSuffix.Length != 0)
-                    {
-                        if (_stopSuffixes.ContainsKey(subSuffix))
-                            return _stopSuffixes[subSuffix];
-                    }
-                    return _stopSuffixes[suffix];
-                }
-                else if(suffix.Count()!=1)
-                {
-                    return _sample.Length;
-                }
-                else
-                {
-                    return _providerStopSymbols[suffix[0]];
-                }
-            }
+            get { return _stopSuffixes[suffix]; }
         }
     }
 }
